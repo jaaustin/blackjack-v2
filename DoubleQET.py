187a4410643698b8aa463ee597d9d6cd5fbbf4e3
jaaustin <james.alastar.austin@gmail.com>
2016-11-04 16:47:15 -0600

    Add files via upload

--- DoubleQET.py
+++ DoubleQET.py
@@ -1,13 +1,22 @@
 import blackjack
 from pylab import *
 from random import *
+import numpy as np
 
 Q1 = [] # NumPy array of correct size
 Q2 = [] # NumPy array of correct size
+ET1 = [] 
+ET2 = []
+newQ1=[]
+newQ2=[]
 
 for i in range(361):
     Q1.append(0.00001*random())
     Q2.append(0.00001*random())
+    ET1.append(0)
+    ET2.append(0)
+    newQ1.append(0)
+    newQ2.append(0)
 
 #print (Q1)
 #print (Q2)
@@ -16,7 +25,7 @@ for i in range(361):
 # Q stick is Q[s], or Q[1] to Q [180]
 # Q hit is Q[s+180], or Q[181] to Q[360]
 
-def learn(alpha, eps, gamma, numTrainingEpisodes):
+def learn(alpha, eps, gamma, decay, numTrainingEpisodes):
     returnSum = 0.0
     for episodeNum in range(numTrainingEpisodes):
         G = 0
@@ -51,9 +60,16 @@ def learn(alpha, eps, gamma, numTrainingEpisodes):
                     amax=1
                     #print("Argmax for Q1 with sprime was hit")
                 #print ("R in the update is",results[0])
-                newQ1 = Q1[s+180*a]+alpha*(results[0]+gamma*Q2[sprime+amax*180]-Q1[s+180*a])
+                delta = (results[0]+gamma*Q2[sprime+amax*180]-Q1[s+180*a])
+                #print(ET1[s+180*a])
+                ET1[s+180*a]=1
+                for x in range(361):
+                    newQ1[x]=Q1[x]+alpha*delta*ET1[x]
+                #newQ1 = Q1[s+180*a]+alpha*(results[0]+gamma*Q2[sprime+amax*180]-Q1[s+180*a])
                 #print ("Q1 for this state and action has been updated from",Q1[s+180*a],"to",newQ1)
-                Q1[s+180*a]=newQ1
+                #Q1[s+180*a]=newQ1
+                ET1[:]=[x*gamma*decay for x in ET1]
+                Q1[:]=[x for x in newQ1]
             else:
                 #print("Chose to update Q2 for this state and action",s,a)
                 if Q2[sprime]>Q2[sprime+180]:
@@ -63,9 +79,16 @@ def learn(alpha, eps, gamma, numTrainingEpisodes):
                     amax=1
                     #print("Argmax for Q2 with sprime was hit")
                 #print ("R in the update is",results[0])
-                newQ2 = Q2[s+180*a]+alpha*(results[0]+gamma*Q1[sprime+amax*180]-Q2[s+180*a])
+                delta = (results[0]+gamma*Q1[sprime+amax*180]-Q2[s+180*a])
+                #print(ET2[s+180*a])
+                ET2[s+180*a]=1
+                for x in range(361):
+                    newQ2[x]=Q2[x]+alpha*delta*ET2[x]
+                #newQ2 = Q2[s+180*a]+alpha*(results[0]+gamma*Q1[sprime+amax*180]-Q2[s+180*a])
                 #print ("Q2 for this state and action has been updated from",Q2[s+180*a],"to",newQ2)
-                Q2[s+180*a]=newQ2
+                #Q2[s+180*a]=newQ2
+                ET2[:]=[x*gamma*decay for x in ET2]
+                Q2[:]=[x for x in newQ2]
             s=sprime
         G=results[0]
         # Fill in Q1 and Q2
